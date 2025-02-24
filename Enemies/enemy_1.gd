c11ab48f05e48c4d1d63de6c467620493e633abe
vctr23 <vsanchezmel23@gmail.com>
2025-02-24 13:39:13 +0100

    recreated map with layers and changed path

--- Enemies/enemy_1.gd
+++ Enemies/enemy_1.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-
+@onready var anim = $AnimatedSprite2D
 @export var speed = 200
 
 func _process(delta):
+	anim.play("walk")
 	get_parent().set_progress(get_parent().get_progress() + speed*delta)
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
